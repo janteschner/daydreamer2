546fe367cfded9a199d3d2759314ac1ee2fa8718
LukeBu95 <lukas.buttke@outlook.de>
2024-01-27 01:30:28 +0100

    Animation for Attacks

--- Assets/Character/AnimController.cs
+++ Assets/Character/AnimController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,13 +8,38 @@ public class AnimController : MonoBehaviour
 
     private Animator animator;
     private CharacterController controller;
-
+    private InputReader input;
 
     // Start is called before the first frame update
     void Start()
     {
         animator = gameObject.GetComponentInChildren<Animator>();
         controller = gameObject.GetComponent<CharacterController>();
+        input = gameObject.GetComponent<InputReader>();
+
+        input.OnWeakPerformed += HandleWeak;
+        input.OnWeakUpPerformed += HandleWeakUp;
+        input.OnWeakSidePerformed += HandleWeakSide;
+        input.OnWeakDownPerformed += HandleWeakDown;
+    }
+
+    private void HandleWeakDown()
+    {
+        animator.SetTrigger("Shoot");
+    }
+
+    private void HandleWeakSide()
+    {
+        animator.SetTrigger("Melee");
+    }
+
+    private void HandleWeakUp()
+    {
+        animator.SetTrigger("Throw");
+    }
+
+    private void HandleWeak()
+    {
     }
 
     // Update is called once per frame
